7f082297b2f3b2ba375819cbbd0100467c37df05
nieweilin11 <78998273+nieweilin11@users.noreply.github.com>
2021-02-23 21:31:52 +0800

    home work

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿#include "std_lib_facilities.h"
+#include <iostream>
+void x(double A, double B, double C)
+{
+	if (B * B - 4 * A * C < 0) throw Fail{};
+	return (-B-sqrt(B*B-4*A*C))/2*A;
+}
+
+
+int main()
+{
+	double a, b, c;
+	try {
+    cin >> a >> b >> c;
+	cout << x(a, b, c) << endl;
+	  }
+	catch (Fail)
+	{
+		cout << "Fail" << endl;
+	}
+}
+
+
+
