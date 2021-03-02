1f56e0ad0658f7c1a59b6711257283fda6958a76
nieweilin11 <78998273+nieweilin11@users.noreply.github.com>
2021-03-02 19:19:26 +0800

    homework week 3

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,24 +1,69 @@
 ﻿#include "std_lib_facilities.h"
-#include <iostream>
-void x(double A, double B, double C)
+double pemuti(double A, double B)
 {
-	if (B * B - 4 * A * C < 0) throw Fail{};
-	return (-B-sqrt(B*B-4*A*C))/2*A;
+	unsigned long long factorial = 1;
+	for (int i = 1; i <= A; ++i)
+	{
+		factorial *= i;
+		A = i;
+	}
+	for (int i = 1; i <= B; ++i)
+	{
+		factorial *= i;
+		B=i;
+	}
+	return A/(A*B);
+	
 }
 
+double combin(double A, double B)
+{
+	unsigned long long factorial = 1;
+	for (int i = 1; i <= A; ++i)
+	{
+		factorial *= i;
+		A = i;
+	}
+	for (int i = 1; i <= B; ++i)
+	{
+		factorial *= i;
+		B = i;
+	}
+	return (A / (A * B)) / B;
+}
 
 int main()
 {
-	double a, b, c;
+	int a, b;
+	int c = 0;
 	try {
-    cin >> a >> b >> c;
-	cout << x(a, b, c) << endl;
-	  }
-	catch (Fail)
+		cout << "Please enter number a and b" << endl;
+		cin >>a>>b;
+		if (a == 0 || b == 0)
+			error("Input number cannot be Zero!");
+		if (a * c != 0 || b * c != 0)
+			throw - 1;
+	     }
+	catch (int e)
 	{
-		cout << "Fail" << endl;
+		cout << "a or b is not a number" << endl;
 	}
+	char op = 'a';
+	cout << "Please choose Permutation  Combination or both of them by enter (P or C or T )" << endl;
+	switch (op) {
+	case'P':
+		cout << "P=" << pemuti(a, b) << endl;
+		break;
+	case'C':
+		cout << "C=" << combin(a, b) << endl;
+		break;
+	case'T':
+		cout << "P=" << pemuti(a, b) << "C=" << combin(a, b) << endl;
+	default:
+		cout << "Don't konw the operation" << endl;
+		break;
+	}
+
+	return 0;
 }
-
-
-
+	   
